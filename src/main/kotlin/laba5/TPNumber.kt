683f727b38f8112e43c90e6e3c316173ff74e9b6
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-10-21 19:11:45 +0700

    Add times, minus

--- src/main/kotlin/laba5/TPNumber.kt
+++ src/main/kotlin/laba5/TPNumber.kt
@@ -1,35 +1,35 @@
 package laba5
 
 class TPNumber {
-    var number: String
+    private var number: String
     var system: Int
     var accuracy: Int
-
+    fun getNumber() : String {
+        val length = number.length
+        if (accuracy > length || accuracy <= 0) {
+            return number // Некорректная позиция, возвращаем исходную строку
+        }
+        return number.substring(0, length - accuracy) + "." + number.substring(length - accuracy)
+    }
+    fun setAccuracy(accuracy: Int) : TPNumber {
+        this.accuracy = accuracy
+        return this
+    }
     constructor(number: String = "0", system: Int = 10, accuracy: Int = 0) {
         require(system in 2..16) { "неподходящаяя система счисления" }
-        this.number = number
+        val arr = number.split('.')
+        if (arr.size == 1)
+        {
+            this.number = number + "0".repeat(accuracy)
+        } else {
+            val fractional = arr[1].take(accuracy)
+            this.number = arr[0] + fractional + "0".repeat(accuracy - fractional.length)
+        }
         this.system = system
         this.accuracy = accuracy
     }
 
-    constructor(number: String, system: String, accuracy: String) :
-            this(
-                number,
-                system.toInt(),
-                accuracy.toInt()
-            )
-
-    fun copy(): TPNumber {
-        return TPNumber(
-            number = this.number,
-            system = this.system,
-            accuracy = this.accuracy
-        )
-    }
-
     operator fun plus(other: TPNumber): TPNumber {
-        require(system == other.system) { "Системы счисления должны быть одинаковыми" }
-        require(accuracy == other.accuracy) { "точности разные" }
 
         val maxLength = maxOf(number.length, other.number.length)
         val num1 = number.padStart(maxLength, '0')
@@ -47,9 +47,72 @@ class TPNumber {
         if (carry > 0) {
             result = digitToChar(carry) + result
         }
-        return TPNumber(result, system )
+        return TPNumber(result, system).setAccuracy(accuracy)
 
     }
+
+    operator fun minus(other: TPNumber) : TPNumber {
+
+        val maxLength = maxOf(number.length, other.number.length)
+        val num1 = number.padStart(maxLength, '0')
+        val num2 = other.number.padStart(maxLength, '0')
+
+        var carry = 0
+        var result = ""
+        for (i in maxLength - 1 downTo 0) {
+            var digit1 = charToDigit(num1[i]) - carry
+            carry = 0
+            var digit2 = charToDigit(num2[i])
+            if (digit2 > digit1)
+            {
+                carry = 1
+                digit1 += system
+            }
+            val sum = digit1 - digit2
+            result = digitToChar(sum) + result
+        }
+        if (carry > 0) {
+            result = digitToChar(carry) + result
+        }
+        return TPNumber(result, system).setAccuracy(accuracy)
+    }
+    operator fun times(other: TPNumber) : TPNumber {
+        //val maxLength = maxOf(number.length, other.number.length)
+        val num1 = number//.padStart(maxLength, '0')
+        val num2 = other.number//.padStart(maxLength, '0')
+
+        var carry = 0
+        var result = ""
+        val resArr = arrayOfNulls<TPNumber?>(num2.length)
+        for (i in num2.length - 1 downTo 0) {
+            for (j in num1.length - 1 downTo 0) {
+                val digit1 = charToDigit(num1[j])
+                val digit2 = charToDigit(num2[i])
+                val sum = digit1 * digit2 + carry
+                carry = sum / system
+                result = digitToChar(sum % system) + result
+            }
+            if (carry > 0) {
+                result = digitToChar(carry) + result
+            }
+
+            val ingexArr = (i-(num2.length-1))*(-1)
+            resArr[ingexArr] = TPNumber(result + "0".repeat(ingexArr), system, 0)
+            result = ""
+        }
+        var rez2 = resArr[0]
+        for(i in 1 .. resArr.size-1)
+        {
+            rez2 = rez2!! + resArr[i]!!
+        }
+
+        return rez2!!.setAccuracy(accuracy*2)
+    }
+    //operator fun div(other: TPNumber) : TPNumber {
+    //    TODO()
+    //}
+
+
     private fun charToDigit(char: Char): Int =
         when (char) {
             in '0'..'9' -> char.digitToInt()
@@ -73,7 +136,4 @@ class TPNumber {
             15 -> 'F'
             else -> throw Exception("не подходяшее число")
         }
-    override fun toString(): String {
-        return "${number} (system $system)"
-    }
 }
--- src/test/kotlin/laba5/TPNumberTest.kt
+++ src/test/kotlin/laba5/TPNumberTest.kt
@@ -3,27 +3,119 @@ package laba5
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 
 class TPNumberTest {
 
-    @Test
-    fun copy() {
+    @Nested
+    inner class ConstructorTests {
+        @Test
+        fun `Constructor 11_11 2 3`() {
+            val num1 = TPNumber("11.11", 2, 3)
+            val rez = num1.getNumber()
+            assertEquals(rez, "11.110")
+        }
 
+        @Test
+        fun ` Constructor 11 2 3`() {
+            val num1 = TPNumber("11", 2, 3)
+            val rez = num1.getNumber()
+            assertEquals(rez, "11.000")
+        }
     }
 
-    @Test
-    fun plus() {
-        // arrange
-        val num1 = TPNumber("101", 2)
-        val num2 = TPNumber("11", 2)
-        val expected = TPNumber("1000",2)
-        // act
-        val actual = num1 + num2
-        // assert
-        assertEquals(expected.number, actual.number)
+    @Nested
+    inner class GetNumberTests {
+        @Test
+        fun ` getNumber 11_11`() {
+            val num1 = TPNumber("11.11", 2, 3)
+            val result = num1.getNumber()
+            assertEquals(result, "11.110")
+        }
     }
 
-    @Test
-    fun testToString() {
+    @Nested
+    inner class PlusTests {
+        @Test
+        fun `plus integers`() {
+            // arrange
+            val num1 = TPNumber("101", 2, 3)
+            val num2 = TPNumber("11", 2, 3)
+            val expected = TPNumber("1000.000", 2, 3).getNumber()
+            // act
+            val a = num1 + num2
+            val actual = a.getNumber()
+            // assert
+            assertEquals(expected, actual)
+        }
+        @Test
+        fun `plus flat`() {
+            // arrange
+            val num1 = TPNumber("1.1", 2, 1)
+            val num2 = TPNumber("1.1", 2, 1)
+            val expected = TPNumber("11.0", 2, 1).getNumber()
+            // act
+            val a = num1 + num2
+            val actual = a.getNumber()
+            // assert
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Nested
+    inner class MinusTests {
+        @Test
+        fun `Minus integers`() {
+            // arrange
+            val num1 = TPNumber("101", 2, 0)
+            val num2 = TPNumber("11", 2, 0)
+            val expected = TPNumber("010", 2, 0).getNumber()
+            // act
+            val a = num1 - num2
+            val actual = a.getNumber()
+            // assert
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun `Minus flat`() {
+            // arrange
+            val num1 = TPNumber("11.1", 2, 1)
+            val num2 = TPNumber("1.1", 2, 1)
+            val expected = TPNumber("10.0", 2, 1).getNumber()
+            // act
+            val a = num1 - num2
+            val actual = a.getNumber()
+            // assert
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Nested
+    inner class TimesTests {
+        @Test
+        fun `Times integers`() {
+            // arrange
+            val num1 = TPNumber("101", 2, 0)
+            val num2 = TPNumber("11", 2, 0)
+            val expected = TPNumber("1111", 2, 0).getNumber()
+            // act
+            val a = num1 * num2
+            val actual = a.getNumber()
+            // assert
+            assertEquals(expected, actual)
+        }
+        @Test
+        fun `Times flat`() {
+            // arrange
+            val num1 = TPNumber("10.1", 2, 1)
+            val num2 = TPNumber("1.1", 2, 1)
+            val expected = TPNumber("11.11", 2, 2).getNumber()
+            // act
+            val a = num1 * num2
+            val actual = a.getNumber()
+            // assert
+            assertEquals(expected, actual)
+        }
     }
 }
