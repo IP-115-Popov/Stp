d26d48956a43fda24cf19fa6425ce05310bbc44d
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-10-27 00:47:47 +0700

    Update addDigitTests , addZeroTests

--- src/test/kotlin/laba6/TEditorTest.kt
+++ src/test/kotlin/laba6/TEditorTest.kt
@@ -90,29 +90,179 @@ class TEditorTest
     @Nested
     inner class addDigitTests{
         @Test
-        fun addDigitReal() {
+        fun addDigitRealInteger() {
             // arrange
-            val expected = "-1.0-1.0i"
+            val expected = "21.0+1.0i"
             // act
             val tEditor =  TEditor("1+1i")
-            val actual = tEditor.addDigit(1,TEditor.Mode.real)
+            val actual = tEditor.addDigit(2,TEditor.Mode.real, TEditor.Part.integer, TEditor.Position.Start)
             // assert
             assertEquals(expected, actual)
         }
         @Test
-        fun addDigitImaginary() {
+        fun addDigitImaginaryInteger() {
             // arrange
-            val expected = "-1.0-1.0i"
+            val expected = "1.0+21.0i"
             // act
             val tEditor =  TEditor("1+1i")
-            val actual = tEditor.addDigit(1,TEditor.Mode.imaginary)
+            val actual = tEditor.addDigit(2,TEditor.Mode.imaginary, TEditor.Part.integer, TEditor.Position.Start)
             // assert
             assertEquals(expected, actual)
         }
+
+        @Test
+        fun addDigitRealImaginary() {
+            // arrange
+            val expected = "1.21+1.0i"
+            // act
+            val tEditor =  TEditor("1.1+1i")
+            val actual = tEditor.addDigit(2,TEditor.Mode.real, TEditor.Part.imaginary, TEditor.Position.Start)
+            // assert
+            assertEquals(expected, actual)
+        }
+        @Test
+        fun addDigitImaginaryImaginary() {
+            // arrange
+            val expected = "1.0+1.21i"
+            // act
+            val tEditor =  TEditor("1+1.1i")
+            val actual = tEditor.addDigit(2,TEditor.Mode.imaginary, TEditor.Part.imaginary, TEditor.Position.Start)
+            // assert
+            assertEquals(expected, actual)
+        }
+
+
+
+        @Test
+        fun addDigitRealIntegerEnd() {
+            // arrange
+            val expected = "12.0+1.0i"
+            // act
+            val tEditor =  TEditor("1+1i")
+            val actual = tEditor.addDigit(2,TEditor.Mode.real, TEditor.Part.integer, TEditor.Position.End)
+            // assert
+            assertEquals(expected, actual)
+        }
+        @Test
+        fun addDigitImaginaryIntegerEnd() {
+            // arrange
+            val expected = "1.0+12.0i"
+            // act
+            val tEditor =  TEditor("1+1i")
+            val actual = tEditor.addDigit(2,TEditor.Mode.imaginary, TEditor.Part.integer, TEditor.Position.End)
+            // assert
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun addDigitRealImaginaryEnd() {
+            // arrange
+            val expected = "1.12+1.0i"
+            // act
+            val tEditor =  TEditor("1.1+1i")
+            val actual = tEditor.addDigit(2,TEditor.Mode.real, TEditor.Part.imaginary, TEditor.Position.End)
+            // assert
+            assertEquals(expected, actual)
+        }
+        @Test
+        fun addDigitImaginaryImaginaryEnd() {
+            // arrange
+            val expected = "1.0+1.12i"
+            // act
+            val tEditor =  TEditor("1+1.1i")
+            val actual = tEditor.addDigit(2,TEditor.Mode.imaginary, TEditor.Part.imaginary, TEditor.Position.End)
+            // assert
+            assertEquals(expected, actual)
+        }
+
     }
     @Nested
     inner class addZeroTests{
+        @Test
+        fun addZeroRealInteger() {
+            // arrange
+            val expected = "1.0+1.0i"
+            // act
+            val tEditor =  TEditor("1+1i")
+            val actual = tEditor.addZero(TEditor.Mode.real, TEditor.Part.integer, TEditor.Position.Start)
+            // assert
+            assertEquals(expected, actual)
+        }
+        @Test
+        fun addZeroImaginaryInteger() {
+            // arrange
+            val expected = "1.0+1.0i"
+            // act
+            val tEditor =  TEditor("1+1i")
+            val actual = tEditor.addZero(TEditor.Mode.imaginary, TEditor.Part.integer, TEditor.Position.Start)
+            // assert
+            assertEquals(expected, actual)
+        }
 
+        @Test
+        fun addZeroRealImaginary() {
+            // arrange
+            val expected = "1.01+1.0i"
+            // act
+            val tEditor =  TEditor("1.1+1i")
+            val actual = tEditor.addZero(TEditor.Mode.real, TEditor.Part.imaginary, TEditor.Position.Start)
+            // assert
+            assertEquals(expected, actual)
+        }
+        @Test
+        fun addZeroImaginaryImaginary() {
+            // arrange
+            val expected = "1.0+1.01i"
+            // act
+            val tEditor =  TEditor("1+1.1i")
+            val actual = tEditor.addZero(TEditor.Mode.imaginary, TEditor.Part.imaginary, TEditor.Position.Start)
+            // assert
+            assertEquals(expected, actual)
+        }
+
+
+
+        @Test
+        fun addZeroRealIntegerEnd() {
+            // arrange
+            val expected = "10.0+1.0i"
+            // act
+            val tEditor =  TEditor("1+1i")
+            val actual = tEditor.addZero(TEditor.Mode.real, TEditor.Part.integer, TEditor.Position.End)
+            // assert
+            assertEquals(expected, actual)
+        }
+        @Test
+        fun addZeroImaginaryIntegerEnd() {
+            // arrange
+            val expected = "1.0+10.0i"
+            // act
+            val tEditor =  TEditor("1+1i")
+            val actual = tEditor.addZero(TEditor.Mode.imaginary, TEditor.Part.integer, TEditor.Position.End)
+            // assert
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun addZeroRealImaginaryEnd() {
+            // arrange
+            val expected = "1.1+1.0i"
+            // act
+            val tEditor =  TEditor("1.1+1i")
+            val actual = tEditor.addZero(TEditor.Mode.real, TEditor.Part.imaginary, TEditor.Position.End)
+            // assert
+            assertEquals(expected, actual)
+        }
+        @Test
+        fun addZeroImaginaryImaginaryEnd() {
+            // arrange
+            val expected = "1.0+1.1i"
+            // act
+            val tEditor =  TEditor("1+1.1i")
+            val actual = tEditor.addZero(TEditor.Mode.imaginary, TEditor.Part.imaginary, TEditor.Position.End)
+            // assert
+            assertEquals(expected, actual)
+        }
     }
     @Nested
     inner class deleteDigitRightTests{
